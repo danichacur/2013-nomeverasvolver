e28f21bbdb6206e8b436b40569563f7520782fc6
Dani Chacur <danichacur@gmail.com>
2013-09-29 20:25:31 -0300

    Agrego la libreria commons a nuestro proyecto

--- commons/Debug/subdir.mk
+++ commons/Debug/subdir.mk
@@ -4,23 +4,41 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/orquestador.c \
-../src/planificador.c 
+../bitarray.c \
+../config.c \
+../error.c \
+../log.c \
+../process.c \
+../string.c \
+../temporal.c \
+../txt.c 
 
 OBJS += \
-./src/orquestador.o \
-./src/planificador.o 
+./bitarray.o \
+./config.o \
+./error.o \
+./log.o \
+./process.o \
+./string.o \
+./temporal.o \
+./txt.o 
 
 C_DEPS += \
-./src/orquestador.d \
-./src/planificador.d 
+./bitarray.d \
+./config.d \
+./error.d \
+./log.d \
+./process.d \
+./string.d \
+./temporal.d \
+./txt.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/%.o: ../src/%.c
+%.o: ../%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -Ipthread -I"/home/utnso/git/tp-2013-2c-nomeverasvolver/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- plataforma/src/orquestador.c
+++ plataforma/src/orquestador.c
@@ -8,6 +8,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Librerias de Commons */
+#include <log.h>
+#include <config.h>
+
+
 int main (){
 
 	printf("hola mundo!!");
