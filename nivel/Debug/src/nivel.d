72c52f1062eaf14356124d04f89bf50c5c7e3b86
matyx17 <matyx17@gmail.com>
2013-10-05 02:29:41 -0300

    subo la primera version del nivel con el código que hicimos en casa la 1era reunion

---
+++ nivel/Debug/src/nivel.d
@@ -0,0 +1,13 @@
+src/nivel.d: ../src/nivel.c \
+ /home/utnso/git/tp-2013-2c-nomeverasvolver/commons/log.h \
+ /home/utnso/git/tp-2013-2c-nomeverasvolver/commons/config.h \
+ /home/utnso/git/tp-2013-2c-nomeverasvolver/commons/collections/dictionary.h \
+ /home/utnso/git/tp-2013-2c-nomeverasvolver/commons/collections/node.h
+
+/home/utnso/git/tp-2013-2c-nomeverasvolver/commons/log.h:
+
+/home/utnso/git/tp-2013-2c-nomeverasvolver/commons/config.h:
+
+/home/utnso/git/tp-2013-2c-nomeverasvolver/commons/collections/dictionary.h:
+
+/home/utnso/git/tp-2013-2c-nomeverasvolver/commons/collections/node.h:
---
+++ nivel/src/nivel.c
@@ -0,0 +1,50 @@
+/*
+ * nivel.c
+ *
+ *  Created on: 05/10/2013
+ *      Author: utnso
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Librerias de Commons */
+#include <log.h>
+#include <config.h>
+
+
+int main (){
+
+	/*printf("hola mundo!!");
+
+	cargarArchivoDeConfiguracion();
+	crearCajasRecursos();
+	conectarAPlataforma(); // implica handShake - envia algoritmo
+	crearHilosEnemigos();
+	crearHiloInterbloqueo();
+	crearHiloInotify();
+	selectConPlataforma();
+	 */
+	return 0;
+}
+/*
+enemigo(){
+	crearseASiMismo(); //random, verifica que no se cree en el (0,0)
+
+	while(1){
+		if(hayPersonajeAtacable){
+			buscaPersonajeCercano();
+			moverseAlternado();
+			actualizarUltimoMovimiento();
+
+			if(estoyArribaPersonaje){
+				avisarAlNivel();
+			}
+		}else{
+			movermeEnL();
+		}
+	}
+}
+*/
+
