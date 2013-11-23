ee0510ddfcbc0aeeef1edfc62fc546f6f8c16fbf
Dani Chacur <danichacur@gmail.com>
2013-11-23 17:15:05 -0300

    Corrijo el tratamiento de desconexiones. Ya se banca el ctrl+c sobre un personaje y la caida de los niveles

--- plataforma/src/planificador.h
+++ plataforma/src/planificador.h
@@ -9,12 +9,14 @@
 #define PLANIFICADOR_H_
 
 #include "orquestador.h"
+#include <sockets/mensajes.h>
 #include <collections/list.h>
 
 typedef struct{
 int32_t nivel;
 int32_t fd;
 char *algol;
+pthread_t pla;
 int32_t quantum;
 int32_t retardo;
 int32_t remain_distance;
@@ -22,6 +24,7 @@ int32_t remain_distance;
 
 void *hilo_planificador(t_niveles_sistema *nivel);
 char * transformarListaCadena(t_list *recursosDisponibles);
+bool plan_enviarMensaje(char* str_nivel, int32_t fd, enum tipo_paquete paquete, char* mensaje);
 
 
 #endif /* PLANIFICADOR_H_ */
