4c9a9b5ca47558614f25cde3c83729d21fb3a5ce
lio1991 <cmi8738>
2013-11-25 01:05:40 -0300

    makefiles

---
+++ plataforma/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/orquestador.c \
+../src/planificador.c 
+
+OBJS += \
+./src/orquestador.o \
+./src/planificador.o 
+
+C_DEPS += \
+./src/orquestador.d \
+./src/planificador.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -Ipthread -I"/home/utnso/git/tp-2013-2c-nomeverasvolver/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
